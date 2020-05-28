d9e0188c3cb501c5925c8e88856c16b80bcae770
denmercs <denmercs@gmail.com>
2020-05-28 15:39:20 -0500

    finish auth flow

---
+++ server/config/dev.js
@@ -0,0 +1,9 @@
+// dev.js don't commit this
+module.exports = {
+  googleClientID:
+    "936233682680-a5iqrg3i4gdkkkvaka2ks0duhikf65l4.apps.googleusercontent.com",
+  googleClientSecret: "zfU8S-JVXt4rL5H7AqIrO8Ls",
+  mongoURI:
+    "mongodb+srv://admin:1234asdf@cluster0-a7xje.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "just a random string",
+};
