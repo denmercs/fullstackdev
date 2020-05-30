aa3f8ef83580008576de64777ce7e7095780ac5e
denmercs <denmercs@gmail.com>
2020-05-29 20:23:27 -0500

    debugging callback error

--- server/index.js
+++ server/index.js
@@ -35,5 +35,5 @@ require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Listening on port:`, PORT);
+  console.log(`Listening on port:::`, PORT);
 });
