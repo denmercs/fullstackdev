94c316fc836c8fe486c5ab8b3cedad6cc0840433
denmercs <denmercs@gmail.com>
2020-05-29 23:02:59 -0500

    initial routes setup

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <>
+      <Router>
+        <Route path="/" exact component={Landing} />
+        <Route path="/Header" component={Header} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/surveyNew" component={SurveyNew} />
+      </Router>
+    </>
+  );
 };
 
 export default App;
