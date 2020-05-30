b7d8d9c2b66d49737c730961a99f0daf42a3aa19
denmercs <denmercs@gmail.com>
2020-05-29 21:45:50 -0500

    solved proxy problem

---
+++ server/client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Sign in here</h3>
+      <a href="/auth/google">Sign in with Google</a>
+    </div>
+  );
+}
+
+export default App;
