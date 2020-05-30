9d4ff6a4d7d68630b31aede148e7899ec64bf3f3
denmercs <denmercs@gmail.com>
2020-05-30 12:08:32 -0500

    done with header and header routes and links

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Header = () => {
+const Header = (props) => {
+  const renderContent = () => {
+    switch (props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  };
   return (
     <>
       <nav>
         <div className="nav-wrapper">
-          <Link to="/" className="left brand-logo">
+          <Link to={props.auth ? "/surveys" : "/"} className="left brand-logo">
             Emaily
           </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="">Login with Google</Link>
-            </li>
-          </ul>
+          <ul className="right">{renderContent()}</ul>
         </div>
       </nav>
     </>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return { auth: state.auth };
+};
+export default connect(mapStateToProps)(Header);
