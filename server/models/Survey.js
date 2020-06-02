bc0da568d4e596b83f78703bc9b89be15115d90b
denmercs <denmercs@gmail.com>
2020-06-02 16:58:48 -0500

    added comment on survey.js

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -9,6 +9,7 @@ const surveySchema = new Schema({
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
+  // the underscore state the relationship field of the User Schema
   _user: { type: Schema.Types.ObjectId, ref: "User" },
   dateSent: Date,
   lastResponded: Date,
