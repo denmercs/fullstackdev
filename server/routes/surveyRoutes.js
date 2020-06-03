51537f7739c21cad04255d5a6229963df154e1db
denmercs <denmercs@gmail.com>
2020-06-02 22:56:23 -0500

    survey routes changes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,37 +1,25 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
-const mongoose = require("mongoose");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
-
-    const survey = new Survey({
-      title,
-      subject,
-      body,
-      recipients: recipients
-        .split(",")
-        .map((email) => ({ email: email.trim() })),
-      _user: req.user.id,
-      dateSent: Date.now(),
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
     });
 
-    // Great place to send an email!
-    const mailer = new Mailer(survey, surveyTemplate(survey));
+    res.send(surveys);
+  });
 
-    try {
-      await mailer.send();
-      await survey.save();
-      req.user.credits -= 1;
-      const user = await req.user.save();
-
-      res.send(user);
-    } catch (err) {
-      res.status(422).send(err);
-    }
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
@@ -64,5 +52,34 @@ module.exports = (app) => {
       .value();
 
     res.send({});
+  });
+
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients
+        .split(",")
+        .map((email) => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+
+    // Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
